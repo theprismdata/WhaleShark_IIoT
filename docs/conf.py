f474ee4f3bd6bdace0c6feb8b64899698751f299
theprismdata <theprismdata@gmail.com>
2021-01-02 22:53:31 +0900

    ..

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, '/Users/prismdata/Documents/1.Data_Centric/3.1.nipa_git/WhaleShark_IIoT/whalesharkM2M')
+sys.path.append('/Users/prismdata/Documents/1.Data_Centric/3.1.nipa_git/WhaleShark_IIoT/whalesharkM2M')
 print('현재 경로', os.getcwd())
 # -- Project information -----------------------------------------------------
 
